4cab7d356f45ea64c7c1afa531ae2e6988818e8b
Thomas Henry <tomjohnhenry@gmail.com>
2020-12-21 19:37:04 -0600

    added button for multi-search version

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -23,12 +23,12 @@ tableData.forEach(function(ufoSighting) {
 
 // Select the button
 var button = d3.select("#filter-btn");
-button.on("click", function() {
+button.on("onclick", function() {
 
     tbody.html("");
 
     // Select the input date get the raw HTML nodes
-    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#input");
     // Get the value property of the input date, state, shape
     var inputValue = inputElement.property("value");
     // console.log input value
